9a09ef43df2511685894e89fdcf74ad9236b9fbb
Mike <mpakleung@gmail.com>
2025-03-19 12:16:01 -0400

    first commit

---
+++ database.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php
+    $servername = "db";
+    $username = "admin";
+    $password = "password";
+    $dbname = "php_docker";
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+
+    // Close the connection
+    $conn->close();
+    ?>
+
+</body>
+
+</html>
---
+++ readme.md
@@ -0,0 +1,19 @@
+This contains the docker compose for a php, apache and mysql project
+
+- Download docker desktop
+- You need the docker desktop to be running before using these commands.
+- Docker compose file also includes php-myadmin
+
+To power on
+```docker-compose up --build```
+
+To power off
+```docker-compose down```
+
+To visit the site, go to [localhost](http://localhost/)
+
+To go to phpmyadmin, [localhost](http://localhost:8001/)
+
+
+Source
+https://www.youtube.com/watch?v=_UEZX-dA-O4
